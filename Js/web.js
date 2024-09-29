4bb73659480cf4dc8a1847ec71fb5e912214c5b4
¨FrancoGatti¨ <fgattidelaconcepcion@gmail.com>
2024-09-29 11:37:09 -0300

    Se agregaron cards

---
+++ Js/web.js
@@ -0,0 +1,15 @@
+/*let confirmar = confirm ("Aceptas los terminos y condiciones de nuestro sitio?");
+
+ let respuesta = prompt ("Que edad tenes?");
+
+let nombre = "Franco";
+let apellido = "Gatti";
+
+
+console.log(`El nombre completo es ${nombre} ${apellido}`);
+
+apellido = "De la Concepción";
+
+console.log(`El nombre completo es ${nombre} ${apellido}`);
+
+*/
