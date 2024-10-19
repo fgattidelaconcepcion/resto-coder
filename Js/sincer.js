405a8b913f2a856a205f8bf8457e829d976e93a2
¨FrancoGatti¨ <fgattidelaconcepcion@gmail.com>
2024-10-19 15:58:56 -0300

    Todo el codigo Js comentado

--- Js/sincer.js
+++ Js/sincer.js
@@ -1,7 +1,7 @@
 const platos = [];
 
 const agregarPlato = () => {
-  const nombrePlato = prompt('Ingrese el nombre del plato').trim();
+  const nombrePlato = prompt('Ingrese el nombre del plato').trim();  //El método trim() elimina los espacios en blanco al principio y al final de una cadena de texto.
 
   if (nombrePlato) {
     const plato = { nombre: nombrePlato};
@@ -14,10 +14,10 @@ const agregarPlato = () => {
 
 const eliminarPlato = () => {
   const platoAEliminar = prompt('Ingrese el nombre del plato que desea eliminar').trim();
-  const index = platos.findIndex(plato => plato.nombre === platoAEliminar);
+  const index = platos.findIndex(plato => plato.nombre === platoAEliminar); //findIndex() es un método que devuelve el índice del primer elemento del array que cumple con una condición especificada en una función.
 
-  if (index !== -1) {
-    platos.splice(index, 1);
+  if (index !== -1) {//Si el indice encontrado es diferente a -1 significa que el elemento existe en el array.
+    platos.splice(index, 1); //splice() es un método que cambia el contenido de un array eliminando o reemplazando elementos existentes. index es la posición del plato que se quiere eliminar y 1 indica que se debe eliminar un solo elemento.
     alert('plato eliminado con éxito');
   } else {
     alert('El plato ingresado no existe');
@@ -45,7 +45,7 @@ const mostrarPlatos = (platos) => {
   if (platos.length === 0) {
     alert('No hay platos en la lista.');
   } else {
-    const listaPlatos = platos.map(plato => `Nombre: ${plato.nombre}`).join('\n');
+    const listaPlatos = platos.map(plato => `Nombre: ${plato.nombre}`).join('\n'); //map()crea un nuevo array de strings que contiene los nombres de todos los platos, cada uno precedido por la palabra "Nombre: ".
     alert(`Lista de Platos:\n${listaPlatos}`);
   }
 };
@@ -78,4 +78,4 @@ const app = () => {
   }
 };
 
-app(); 
+app(); //Controla el bucle para que el usuario pueda seleccionar las opciones.
